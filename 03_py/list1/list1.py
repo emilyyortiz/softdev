57537477fc6cfa15bf39c0c3f601a06d6cafce48
Emily Ortiz <78173159+eortiz-30@users.noreply.github.com>
2022-09-21 12:53:55 -0400

    completed list1, need comments and test cases

--- 03_py/list1/list1.py
+++ 03_py/list1/list1.py
@@ -22,4 +22,33 @@ def rotate_left3(nums):
   
 def reverse3(nums):
   return [nums[2], nums[1], nums[0]]
-  
+
+def max_end3(nums):
+  #finding the larger value
+  bigger = max(nums[0], nums[-1])
+  #Loop for replacing vals
+  for i in range(len(nums)):
+    nums[i] = bigger
+  return nums
+
+def sum2(nums):
+  #special case 0
+  if len(nums) == 0: 
+    return 0
+  #special case 1
+  elif len(nums) == 1:
+    return nums[0]
+  else :
+    return nums[0] + nums[1]
+
+def middle_way(a, b):
+  # middle element has index 1
+  return [a[1], b[1]]
+
+def make_ends(nums):
+  # -1 is index of last element
+  return [nums[0], nums[-1]]
+
+def has23(nums):
+  # in determines if an element is in an array
+  return 2 in nums or 3 in nums
