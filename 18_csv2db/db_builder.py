dc79584d291010cb4945677d40c76f364f28d63e
eortiz-30 <eortiz30@stuy.edu>
2022-10-26 12:42:02 -0400

    adF

--- 18_csv2db/db_builder.py
+++ 18_csv2db/db_builder.py
@@ -12,22 +12,30 @@ DB_FILE="discobandit.db"
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
 
+c.execute("DROP TABLE if exists students")
+
 c.execute("CREATE TABLE students(name TEXT, age INTEGER, id INTEGER)")
 
 with open("students.csv", newline='') as csvfile: # open() as __ SAME NAME BELOW
     students_reader = csv.reader(csvfile, delimiter=",") # csv.reader(__, )
+    # print(students_reader)
     for row in students_reader:
-        print(row)
-        c.execute("INSERT into students values(name, age, id)")
+        # print(row)
+        c.execute("INSERT into students VALUES()")
 
 print("about to print students database")
-c.execute("SELECT * FROM students")
 
 command = ""          # test SQL stmt in sqlite3 shell, save as string
 c.execute(command)    # run SQL statement
 
 db.commit() #save changes
+
+ex = c.execute("SELECT * FROM students")
+fetch = ex.fetchall()
+
 db.close()  #close database
+
+print(fetch)
 
 # drop tables (CHECK OUT LATER)
 # allows you to edit a table existing already/checks if it's there already
