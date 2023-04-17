ba668839ccb0848008ec9b514998af5007bc3adb
eortiz-30 <eortiz30@stuy.edu>
2023-04-17 13:55:37 -0400

    27 done

--- 27_js/fibfac.js
+++ 27_js/fibfac.js
@@ -1,7 +1,7 @@
-// Team Phantom Tollbooth :: Clyde Sinclair, Fierce Dragon 
-// SoftDev pd0
-// K27 -- Basic functions in JavaScript
-// 2023-04-04t
+//Elmo :: Ryan Lee, Emily Ortiz
+//SoftDev pd8
+//K27 -- Basic functions in JavaScript
+//2023-04-03t
 // --------------------------------------------------
 
 
@@ -11,3 +11,32 @@
 
 
 //Do whatever you think is needed. Think: S I M P L E.   Think: S M A R T.
+
+function fact(n){
+    if (n < 2){
+        return 1;
+    } 
+    return n * fact(n - 1); 
+}
+
+// console.log(fact(1)); // 1
+// console.log(fact(2)); // 2
+// console.log(fact(3)); // 6
+// console.log(fact(4)); // 24
+// console.log(fact(5)); // 120
+
+function fib(n){
+    if (n == 0){
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else{
+        return fib(n - 2) + fib(n - 1);
+    }
+}
+
+console.log(fib(0)); // 0
+console.log(fib(1)); // 1
+console.log(fib(2)); // 1
+console.log(fib(3)); // 2
+console.log(fib(4)); // 3
